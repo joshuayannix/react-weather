73e57fca29cd3a1356bb3c7810fbd2888e5f0cc8
joshuayannix <joshuayannix@gmail.com>
2020-10-03 13:02:16 -0400

    api calls lifecycle working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: null,
+      api: '',
+      temp: ''
+    };    
+  };
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(position => {
+      let long = position.coords.longitude;
+      let lat = position.coords.latitude;
+      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ad4e5a45e55e91999a3bf297f3a10367`;
+      this.setState({ api: api})
+      this.getWeather()
+    });
+  }
+
+  async getWeather() {
+    try { 
+      const result = await fetch(this.state.api);
+      const data = await result.json();
+      console.log(data.main.temp);
+      this.setState({ temp: data.main.temp })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  
+
+  render() {
+    return(
+      <div className='grey'>
+        weather
+        {this.state.temp}
+      </div>
+    )
+  }
 }
 
 export default App;
